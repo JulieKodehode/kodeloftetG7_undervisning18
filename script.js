590962c4350645f8b1d8027ec7677636de35199b
JulieKodehode <julie.kodehode@gmail.com>
2024-04-25 12:01:58 +0200

    Undervisnng 20: Fullført prosjekt

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+  /*
+  - An event listner listening for a click on a button
+  - Event listener activates a function that runs everytime the button is clicked
+  - Get value data from the HTML input and save it in an array
+  - Loop through the array and make an li-element for each item in the array
+  - Sends the filled li-elements back to the div-element in HTML
+  - Created a button that deletes the todo when finished
+  */
+    
     // Array without content
     const todoTasks = [];
 
@@ -25,9 +34,36 @@
         // Create a new HTML-element 'li'
         const newListItem = document.createElement("li");
         console.log(newListItem);
+
+        // Add a class to the new li-element
+        newListItem.classList.add("taskListItem");
         
         // Set that the element 'li' have content from our looped through data
         newListItem.textContent = newTodoTask;
+
+        // Created a new HTML-element
+        const deleteButton = document.createElement("button");
+        console.log(deleteButton);
+
+        // Given the new element some text content
+        deleteButton.textContent = "Delete todo";
+
+        // Added function to the new button-element
+        deleteButton.addEventListener("click", function () {
+            console.log("Click?")
+            newListItem.remove();
+            deleteButton.remove();
+        })
+
+        // Getting the div-element from the HTML
+        const taskListContainer = document.querySelector("#taskListContainer");
+        console.log(taskListContainer);
+
+        // Returns the newly created li-element to the div-element
+        taskListContainer.appendChild(newListItem);
+
+        // Returns the newly created button-element to the div-element
+        taskListContainer.appendChild(deleteButton);
     }
 }
 
